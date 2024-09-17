8d56416afae906129c38ced719b0bb57bfd3d6dc
Murillo Martins <murillo.lemes@escola.pr.gov.br>
2024-09-17 08:25:52 -0300

    Add files via upload

---
+++ principal.js
@@ -0,0 +1,7 @@
+var titulo = document.querySelector(".titulo");
+titulo.textContent = "Aparecida Nutricionista";
+var paulo = document.querySelector('#linhaPaulo');
+var pesoPaulo = paulo.querySelector(".info-peso").textContent;
+var alturaPaulo = paulo.querySelector(".info-altura").textContent;
+var imcPaulo = pesoPaulo / (alturaPaulo*alturaPaulo);
+paulo.querySelector(".info-imc").textContent = imcPaulo;
